85e7b217abf15980612953c067487c12846eda5e
SAYEED <90544352+Sayeed12345@users.noreply.github.com>
2022-02-16 00:34:29 +0530

    Add files via upload

---
+++ reactfood/reactfood/src/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Restaurant from './components/Basics/Restaurant';
+
+const App = () => {
+  return (
+    <div>
+      <h1></h1>
+      <Restaurant />
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import React from 'react';
+
+// // const App = () => {
+// //   return <div>
+// //     Hello world
+// //   </div>;
+// // };
+
+
+
+
+// const App = () => {
+//   return (
+//     <>
+//       <h1>hello hi...{3 + 563}</h1>
+//       <p>hwywhwy</p>
+//       <img src="" alt="" />
+//       <MyName />
+//     </>
+//   );
+// };
+
+// const MyName = () => {
+//   return (
+//     <div>
+//       <h1>Sayeed</h1>
+//     </div>
+//   )
+// }
+
+// export default App;
